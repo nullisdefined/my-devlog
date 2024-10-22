ed88966017d9391a9d56b0ffc1662af665e4e848
nullisdefined <jaeuu.dev@gmail.com>
2024-10-22 22:08:17 +0900

    fix: 메인페이지 타이틀 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { ThemeProvider } from "./context/theme-provider";
 
 export const metadata: Metadata = {
-  title: "김재우의 포트폴리오 & 블로그",
-  description: "풀스택 개발자 김재우의 포트폴리오와 기술 블로그입니다.",
+  title: "Main",
+  description: "메인 페이지 입니다.",
 };
 
 export default function RootLayout({
