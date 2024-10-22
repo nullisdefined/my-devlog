259c38169d3d88d3e48fc43a0ffc95c1a1cbbab5
nullisdefined <jaeuu.dev@gmail.com>
2024-10-22 16:00:46 +0900

    chore: next config 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // next.js를 HTML로 내보내기 위한 설정
   basePath: "/next-blog",
   assetPrefix: "/next-blog/",
   images: {
